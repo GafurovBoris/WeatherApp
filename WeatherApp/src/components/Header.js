b933162a32aead7f78dbc2553e360847b0f120b4
GafurovBoris <gafuroff.boris@yandex.ru>
2024-05-07 18:57:08 +0500

    Initial commit

---
+++ WeatherApp/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "../styles/Header.css";
+
+
+ function Header() {
+        return (
+            <header><i>Прогноз погоды</i></header>
+        );
+}
+
+export default Header;
